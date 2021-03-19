6b61d86eacbadf837e154fa2a63f9caed89dc4a5
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-19 09:28:50 -0500

    Implement basic router structure for three resources and ensure they are live

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,20 @@
-// build your `/api/projects` router here
+const router = require('express').Router();
+// import middleware
+const Project = require('./model');
+
+router.get('/', (req, res, next) => {
+  res.json({msg: "get to be implemented"})
+});
+
+router.post('/', (req, res, next) => {
+  res.json({msg: "post to be implemented"})
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    error: err.message,
+    stack: err.stack,
+  })
+});
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,20 @@
-// build your `/api/tasks` router here
+const router = require('express').Router();
+// import middleware
+const Task = require('./model');
+
+router.get('/', (req, res, next) => {
+  res.json({msg: "get to be implemented"})
+});
+
+router.post('/', (req, res, next) => {
+  res.json({msg: "post to be implemented"})
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    error: err.message,
+    stack: err.stack,
+  })
+});
+
+module.exports = router;
