89cdbbb6c0f5eb7b080092519727d69965a756cb
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-19 10:16:33 -0500

    Implement POST resources

--- api/resource/model.js
+++ api/resource/model.js
@@ -4,8 +4,13 @@ const getAll = () => {
   return db('resources');
 };
 
-const create = (resource) => {
+const create = async (resource) => {
+  const [id] = await db('resources').insert(resource, ['resource_id']);
+  return getById(id);
+};
 
+const getById = (resource_id) => {
+  return db('resources').where({ resource_id }).first();
 };
 
 module.exports = {
